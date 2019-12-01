4812cd54157ec712674379b82e91503f1aa0c28d
cksstar123 <58160050+cksstar123@users.noreply.github.com>
2019-12-01 18:09:39 +0900

    test

---
+++ test/number/src/number/mainClass.java
@@ -0,0 +1,10 @@
+package number;
+
+public class mainClass {
+	public static void main(String[] args) {
+		int i = 10;
+		int j;
+		j=100;
+		System.out.println((i+j));
+	}
+}
---
+++ test/test1/src/test1/Mainclass.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Mainclass {
+public static void main(String[] args) {
+	/*
+	 * 특수문자
+	 * 주석
+	 */
+	System.out.println("Good\tMorning~");
+	System.out.println("Good\nMorning~");
+	System.out.println("Good\'Morning~\'");
+	System.out.println("Good\"Morning~\"");
+	System.out.println("Good\\Morning~\\");
+	
+	}
+}
